4873170951f246929f6588c3cd5123a8e2f24e75
Bryan <bwilson.12@icloud.com>
2022-05-12 22:28:55 -0400

    Database Edit

--- FamilyWeb/settings.py
+++ FamilyWeb/settings.py
@@ -84,7 +84,8 @@ WSGI_APPLICATION = 'FamilyWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-       
+       'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
