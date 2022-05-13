383d2e38313a9946f275074a56ed847ba50df0e9
Bryan <bwilson.12@icloud.com>
2022-05-12 20:58:08 -0400

    Eliminate mysql

--- FamilyWeb/settings.py
+++ FamilyWeb/settings.py
@@ -83,6 +83,7 @@ WSGI_APPLICATION = 'FamilyWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -94,6 +95,7 @@ DATABASES = {
 
     }
 }
+'''
 
 
 # Password validation
