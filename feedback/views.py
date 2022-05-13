4e3bb4aadf59b4507aeaa7428a23e61eff156602
Bryan <bwilson.12@icloud.com>
2022-05-12 21:46:10 -0400

    Delete Feedback and Polls

--- feedback/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def index(request):
-    return render(request, 'feedback/index.html') #when this function is called form the urls page, this page is rendered using the index.html template
