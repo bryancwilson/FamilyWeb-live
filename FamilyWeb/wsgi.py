302533dfcc41680877b0385d5f8e99ee1ee50bb8
Bryan <bwilson.12@icloud.com>
2022-05-22 14:57:49 -0400

    edit wsgi again

--- FamilyWeb/wsgi.py
+++ FamilyWeb/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import sys
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FamilyWeb.settings')
-
-application = get_wsgi_application()
+path = os.path.expanduser('~/mysite')
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
