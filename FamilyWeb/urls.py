d88be2d2682ec10ae45e8faf2670ca7ba2f781a8
Bryan <bwilson.12@icloud.com>
2022-05-22 19:12:30 -0400

    change home url in FamilyWeb folder

--- FamilyWeb/urls.py
+++ FamilyWeb/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('home/', include('home.urls')),
+    path('', include('home.urls')),
     path('contact/', include('contact.urls')),
     path('agenda/', include('agenda.urls')),
     path('registration/', include('registration.urls')),
